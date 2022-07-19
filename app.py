6a95ef406dc9ae53b7e90a077a4e18b81fe2fce0
Nithin Pradeep <nithin.pradeep91@gmail.com>
2022-07-18 23:20:38 -0700

    logger tested

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from crypt import methods
 from flask import Flask
+import sys
+from housing.logger import logging
 
 app= Flask(__name__)
 
 @app.route("/", methods= ['GET', 'POST'])
 
 def index():
+    logging.info("Testing logging")
     return "CI CD testing"
 
 
