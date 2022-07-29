20d8569f7b63b2a79502d3616c60d1355f5ee402
Nithin Pradeep <nithin.pradeep91@gmail.com>
2022-07-29 14:04:53 -0700

    Added EDA

--- housing/entity/artifact_entity.py
+++ housing/entity/artifact_entity.py
@@ -6,3 +6,13 @@ DataIngestionArtifact = namedtuple("DataIngestionArtifact",
 
 DataValidationArtifact = namedtuple("DataValidationArtifact",
 ["schema_file_path","report_file_path","report_page_file_path","is_validated","message"])
+
+DataTransformationArtifact = namedtuple("DataTransformationArtifact",
+ ["is_transformed", "message", "transformed_train_file_path","transformed_test_file_path",
+     "preprocessed_object_file_path"])
+
+"""
+transformed test and train file path are locations wehere I store the transformed test and train files.
+When the pickle of data transformation steps is created, it is saved at preprocessed object file path.
+
+"""
